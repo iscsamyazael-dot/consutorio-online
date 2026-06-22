8ca63ef1973443d8c986510dd81e25033491ea3a
Xander <yt0718609@gmail.com>
2026-06-22 14:21:07 -0600

    nada solo un cambio

--- config/adminlte.php
+++ config/adminlte.php
@@ -418,7 +418,7 @@ return [
                 [
                     'text' => 'Historial de recetas',
                     'icon' => 'fas fa-fw fa-book-medical',
-                    'url' => 'medico/recetas',
+                    'url' => 'medico/HistorialRecetas',
                 ],
             ],
         ],
