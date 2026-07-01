fe4da71df42cfde146121aeef60517af7045abe3
gaelesquivelkoh1-lang <gaelesquivelkoh1@gmail.com>
2026-07-01 11:05:58 -0600

    cambio de interfaz

---
+++ resources/views/Citas/index.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte::page')
+
+@section('title', 'Agenda Médica')
+
+@section('content_header')
+<input type="hidden" name="route" value="{{ url('/') }}">
+    
+@stop
+@section('content')
+<div id="app">
+    <masteragenda></masteragenda>
+</div>
+@stop
+@section('js')
+@vite('resources/js/app.js')
+@stop
+    
