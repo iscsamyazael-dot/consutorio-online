2bf3ba5ccdc6890e9fa8f1c342806eb7f78523ad
samyazael <tsuazael@gmail.com>
2026-07-09 12:37:12 -0600

    Se comento la linea de CheckRole del archiivo app.php - Bootstrap
    Se comento el archivo de CheckRole para dejar unicamente el can

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-    $middleware->alias([
-        'rol' => \App\Http\Middleware\CheckRole::class, 
-    ]);
+        // $middleware->alias([
+        //     'rol' => \App\Http\Middleware\CheckRole::class, 
+        // ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
