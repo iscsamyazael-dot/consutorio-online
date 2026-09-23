b60f482cfa442fa96f599d7c52488eb1fde1c527
samyazael <tsuazael@gmail.com>
2026-09-23 09:31:31 -0600

    Actualizacion de los modelos Clinica Trabajo

--- app/Models/ClinicaTrabajo/HistorialPuesto.php
+++ app/Models/ClinicaTrabajo/HistorialPuesto.php
@@ -5,6 +5,8 @@ namespace App\Models\ClinicaTrabajo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Paciente;
+use App\Models\Medico;
 
 class HistorialPuesto extends Model
 {
